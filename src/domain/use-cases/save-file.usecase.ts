119f6509c86f3692fbcd14ff26409cc747d2ca3f
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-11-27 19:04:22 -0300

    save file use case

---
+++ src/domain/use-cases/save-file.usecase.ts
@@ -0,0 +1,30 @@
+import fs from 'fs';
+
+export interface SaveFileUseCase {
+  execute: (opstions: SaveFileOptions) => boolean;
+}
+
+export interface SaveFileOptions {
+  fileContent: string;
+  fileDestination?: string;
+  fileName?: string;
+}
+
+export class SaveFile implements SaveFileUseCase {
+  constructor() {}
+
+  execute({
+    fileContent,
+    fileDestination = 'outputs',
+    fileName = 'table',
+  }: SaveFileOptions): boolean {
+    try {
+      fs.mkdirSync(fileDestination, { recursive: true });
+      fs.writeFileSync(`${fileDestination}/${fileName}.txt`, fileContent);
+      return true;
+    } catch (error) {
+      console.error(error)
+      return false;
+    }
+  }
+}
--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -1,4 +1,5 @@
 import { CreateTable } from '../domain/use-cases/create-table.usecase';
+import { SaveFile } from '../domain/use-cases/save-file.usecase';
 
 interface RunOptions {
   base: number;
@@ -10,6 +11,13 @@ export class ServerApp {
   static run({ base, limit, show }: RunOptions) {
     console.log('Server running...');
     const table = new CreateTable().execute({ base, limit });
+    const wasCreated = new SaveFile().execute({
+      fileContent: table,
+      fileDestination: `outputs/table-${base}`,
+    });
     show && console.log(table);
+    wasCreated
+      ? console.log('File created!')
+      : console.log('File not created!');
   }
 }
